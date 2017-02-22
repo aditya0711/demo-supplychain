f81d32ce8ac48923d1feb8812aa57c7972348d1e
Gunjan07 <Gunjan_94>
2017-02-23 01:47:28 +0800

    [Fixes #138396613 ]
    Minor fix in route file

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,8 @@ import React from 'react';
 import addProduct from'./AddProduct/container/addProduct';
 import {Route,  IndexRoute} from 'react-router';
 
-import AddProduct from './AddProduct';
-import ProductList from './ProductsList';
-
 export default (
     <Route path="/">
             <IndexRoute component={addProduct} />
-        <Route path="/list" component={ProductList}></Route>
     </Route>
 );
