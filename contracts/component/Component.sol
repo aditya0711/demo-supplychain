8ce46f8889ff310acd28abb0eccb87c4e2d39e82
Lior Saar <liorsaar1@gmail.com>
2017-02-22 16:04:54 -0800

    addComponent

--- contracts/component/Component.sol
+++ contracts/component/Component.sol
@@ -2,14 +2,27 @@
  * Component
  */
 contract Component {
-  // note: using byte32, since Solidity can't return variable length strings
-   bytes32 _id32;
-   string _id;
-   string _name;
+  bytes32 _id32;
+  string _id;
+  string _name;
+
+  struct subComponent {
+    bytes32 id32;
+    string id;
+    uint quantity;
+  }
+
+  subComponent[] children;
+  mapping (bytes32 => uint) idToComponentMap;
 
   function Component(bytes32 id32, string id, string name) {
     _id32 = id32;
     _id = id;
     _name = name;
+  }
+
+  function addComponent(bytes32 id32, string id, uint quantity) {
+    idToComponentMap[id32] = children.length;
+    children.push(subComponent(id32, id, quantity));
   }
 }
