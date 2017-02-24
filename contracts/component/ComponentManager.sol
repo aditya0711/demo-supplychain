45a33d69388ec10457f381a1e5652a6e4adba479
Lior Saar <liorsaar1@gmail.com>
2017-02-23 18:37:53 -0800

    add sub component

--- contracts/component/ComponentManager.sol
+++ contracts/component/ComponentManager.sol
@@ -19,12 +19,26 @@ contract ComponentManager is ErrorCodes {
     return idToComponentMap[id32] != 0;
   }
 
-  function addComponent(bytes32 id32, string id, string name) returns (ErrorCodesEnum) {
+  function createComponent(bytes32 id32, string id, string name) returns (ErrorCodesEnum) {
     // fail if id32 exists
     if (exists(id32)) return ErrorCodesEnum.EXISTS;
     // add component
     idToComponentMap[id32] = components.length;
-    components.push(new Component(id32, id, name));
+    Component component = new Component(id32, id, name);
+    components.push(component);
     return ErrorCodesEnum.SUCCESS;
   }
+
+  function addSubComponent(bytes32 parentId32, bytes32 childId32, string childId, uint quantity) returns (ErrorCodesEnum) {
+    // fail if parent doesnt exist
+    if (!exists(parentId32)) return ErrorCodesEnum.NOT_FOUND;
+    // fail if child doesnt exist
+    if (!exists(childId32)) return ErrorCodesEnum.NOT_FOUND;
+    // add sub component
+    uint index = idToComponentMap[parentId32];
+    Component parent = components[index];
+    return parent.addSubComponent(childId32, childId, quantity);
+  }
+
+
 }
