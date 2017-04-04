f7c93834d7a2fc5bd756a4e6837584200a32cbec
aditya0711 <aditya071193@gmail.com>
2017-04-04 10:05:42 +0800

    [Fixes #138396613] React: CreateProductSuccess works

--- src/components/CreateProductSuccess.js
+++ src/components/CreateProductSuccess.js
@@ -6,15 +6,8 @@ import { connect } from 'react-redux';
 
 class CreateProductSuccess extends Component {
 
-    componentDidMount() {
-        console.log("CREATE PRODUCT SUCCESS COMPONENT PROPS: " + JSON.stringify(this.props));
-    }
-    componentWillUpdate(){
-        console.log("COMPONENT UPDATE" + JSON.stringify(this.props));
-    }
-
     render() {
-        if(this.props.products.success === 'true') {
+        if(this.props.products.success === true) {
             return (
                 <div>
                     <h1>PRODUCT ADDED SUCCESSFULLY</h1>
@@ -23,7 +16,6 @@ class CreateProductSuccess extends Component {
         }
         return(
             <div>
-                <h1>SOMETHING</h1>
             </div>
         )
     }
